4136647f97571b118864aa687e4c6e34f89e84bf
yingyangcode <marcel.anis.21@gmail.com>
2020-02-10 23:45:21 -0500

    Parsing request paths

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
+/**
+* Primary file for the api
+*/
 
-console.log("Hello World");
+// Dependencies
+const http = require('http');
+const url = require('url');
+
+// The server should respond to all requests with a string
+const server = http.createServer(function(req, res){
+
+    // Get the url and parse it
+    // True means parse the query string and send it to the query string module
+    // url module has the ability to call the queryString module with this boolean flag
+    const parsedUrl = url.parse(req.url, true);
+
+    // Get the path
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+    // Send the response
+    res.end('Hello World\n');
+
+    // Log the request path
+    console.log('Request is received on path: '+trimmedPath);
+
+    
+});
+
+// Start the server and have it listen on port 3000
+server.listen(3000, function() {
+    console.log("The server is listening on port 3000 now");
+});
+
 
 
