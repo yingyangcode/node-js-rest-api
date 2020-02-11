a409f78d8f23f92677f6b18001d8f671021661e8
yingyangcode <marcel.anis.21@gmail.com>
2020-02-11 18:46:51 -0500

    Routing requests

--- index.js
+++ index.js
@@ -35,14 +35,47 @@ const server = http.createServer(function(req, res){
     req.on('data', function (data){
         buffer += decoder.write(data);
     });
+    // 'end' event runs on every request
     req.on('end', function(){
         buffer += decoder.end();
-    
+        
+        // Choose the handler this request should go to
+        // If one is not found, use the notFound handler
+        const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+        
+        // Construct the data object to send to the handler
+        const data = {
+            'trimmedPath' : trimmedPath,
+            'queryStringObject' : queryStringObject,
+            'method' : method,
+            'headers' : headers,
+            'payload' : buffer
+        };
+
+        // Route the request to the handler specified in the router
+        chosenHandler(data, function(statusCode, payload){
+            // Use the status code called back by the handler, or default to 200
+            statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+            // Use the payload called back by the handler, or default to an empty object
+            payload = typeof(payload) == 'object' ? payload : {};
+
+            // Convert the payload to a string
+            const payloadString = JSON.stringify(payload);
+
+            // Return the response
+            res.writeHead(statusCode);
+            res.end(payloadString);
+
+            // Log the response
+            console.log('Returning this response: ', statusCode, payloadString );
+        })
+
         // Send the response
-        res.end('Hello World\n');
+        // res.end('Hello World\n');
 
         // Log the request path
-        console.log('Request received with payload:', buffer);
+        // console.log('Request received with payload:', buffer);
         //console.log('Request received with these headers', headers);
         //console.log('Request is received on path: '+trimmedPath+' with method: '+method+ ' with these query string parameters: '+ JSON.stringify(queryStringObject));
     });
@@ -53,5 +86,20 @@ server.listen(3000, function() {
     console.log("The server is listening on port 3000 now");
 });
 
+// Define the handlers
+const handlers = {};
 
+// Sample handler
+handlers.sample = function(data, callback) {
+    // callback a http status code and a payload object
+    callback(406, {'name': 'sample handler'});
+};
+// Not found handler
+handlers.notFound = function(data, callback) {
+    callback(404);
+};
+// Define a request router
+const router = {
+    'sample': handlers.sample
+};
 
