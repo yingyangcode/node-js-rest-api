2adcf673dfdf0c1d9892d330d819d8b3ce92355f
yingyangcode <marcel.anis.21@gmail.com>
2020-02-13 21:01:54 -0500

    remove twilio test

--- index.js
+++ index.js
@@ -12,10 +12,7 @@ const fs = require('fs');
 const handlers = require('./lib/handlers');
 const helpers = require('./lib/helpers');
 
-// @TODO get rid of this
-helpers.sendTwilioSms('4158375309', 'Hello!', function(err){
-    console.log('this was the error',err);
-});
+
 
 // Instantiating the http server
 const httpServer = http.createServer(function(req, res){
